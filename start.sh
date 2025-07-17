6b8b001bf057359dc4d680b63e7b4124a0e4abd4
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-07-17 17:56:28 +0530

    script

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd /home/ubuntu/atd/attendanceSystemB
+
+cd /home/ubuntu/atd/attendanceSystemB || exit
 source .venv/bin/activate
+
+# Apply migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Start the server
 python manage.py runserver 0.0.0.0:8004
