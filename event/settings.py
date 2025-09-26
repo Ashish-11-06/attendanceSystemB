5cc766010ea87a7eeea5982e4bcb2382f864c9ac
Shweta Bamane <shwetabbamane@gmail.com>
2025-09-26 11:46:03 +0530

    add csrf

--- event/settings.py
+++ event/settings.py
@@ -20,6 +20,12 @@ DEBUG = True  # ⚠️ Set to False in production
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://sncf-api.prushal.com",
+    "https://sncf.prushal.com",
+]
+
+
 # Application definition
 INSTALLED_APPS = [
     'corsheaders',
